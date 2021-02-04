8310c394368aa74f3826c1efd37dc01e5c97b71a
FernandoGherson <fghersonf@gmail.com>
2021-02-04 23:50:17 +0200

    Input system + Randomizer
    - Imported the Input System
    using presses to make action
    
    - Crated the Randomizer
    UsingDice() - take a random number (1 - 6)
    Using(int{})- take a random number from the given array

---
+++ Double Jack Project/Assets/Scripts/Player.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{ 
+    
+
+    public void Click(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            Debug.Log("Dice =" + Randomizer.UsingDice().ToString());
+        }    
+    }
+}
---
+++ Double Jack Project/Assets/Scripts/Randomizer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public static class Randomizer
+{
+    public static int UsingDice()
+    {
+        return Random.Range(1, 7);
+    }
+
+    public static int Using(int[] numbersToUse)
+    {
+        int index = Random.Range(0, numbersToUse.Length);
+        return numbersToUse[index];
+    }
+}
